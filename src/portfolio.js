83d0e883fde75e85ff561a7b10559012bea4152d
Shradha Parkhi <62986288+shradha1612@users.noreply.github.com>
2021-02-24 23:54:36 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,15 +1,14 @@
 // Social Media Links
-
 const socialMediaLinks = {
-    github: 'https://github.com/saadpasta',
-    linkedin: 'https://www.linkedin.com/in/saadpasta/',
-    gmail: 'saadpasta70@gmail.com',
-    gitlab: 'https://gitlab.com/saadpasta',
-    facebook: 'https://www.facebook.com/saad.pasta7',
-    medium: 'https://medium.com/@saadpasta',
-    stackoverflow: 'https://stackoverflow.com/users/10422806/saad-pasta',
+    github: 'https://github.com/shradha1612',
+    linkedin: 'https://www.linkedin.com/in/shradha-parkhi-06054b191/',
+    gmail: 'parkhishradha@gmail.com',
+    gitlab: 'https://gitlab.com/Shradha1612',
+    medium: 'https://codewebtech.medium.com/',
+    instagram:"https://instagram.com/codewebtech/",
+    twitter:"https://twitter.com/@codewebtech_",
     // Instagram and Twitter are also supported in the links!
     display: true, // Set true to display this section, defaults to false
   };
-  
+
 export default socialMediaLinks;
